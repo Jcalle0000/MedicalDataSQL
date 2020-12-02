f7ad33b2c68aab34f772be8d919947ead5cc15bd
Jason <jcalle0000@gmail.com>
2020-12-02 14:07:29 -0500

    selected data with the different diseases and age requirements

--- CREATETABLES.sql
+++ CREATETABLES.sql
@@ -3,17 +3,42 @@ CREATE TABLE CUSTOMER_INFO
 (
     customer_id     VARCHAR(20)     NOT NULL PRIMARY KEY, -- ID HAS LETTERS
     employment      VARCHAR(250),
-    education       VARCHAR(250)
+    education       VARCHAR(250),
+
+    -- this could all be turned into a combination
+    -- with an identity but would make 122,880 combination
+
+    martial         VARCHAR(15), -- Married, Separated, Divorced, Widowed, Never Married 
+    gender          VARCHAR(15), -- Male, Female
+    age             VARCHAR(15),
+    seniorCitizen   VARCHAR(15),-- From here its all yes/no
+    highBlood       VARCHAR(15),
+    stroke          VARCHAR(15),
+    complication_risk VARCHAR(15),
+    overweight      VARCHAR(15),
+    arthritis      VARCHAR(15),
+    diabetes        VARCHAR(15),
+    hyperlipidemia      VARCHAR(15),
+    backpain      VARCHAR(15),
+    anxiety      VARCHAR(15),
+    allergic_rhinitis   VARCHAR(15),
+    reflux_esophagitis  VARCHAR(15),
+    asthma          VARCHAR(15)
 )
 GO
 
+select distinct martial from INPUT_DATA
+
 INSERT INTO CUSTOMER_INFO
     SELECT customer_id, employment, education
-    FROM INPUT_DATA
+    , martial, gender,age , seniorCitizen, highBlood, stroke
+    , complication, overweight, arthritis, diabetes, hyperlipidemia
+    , backPain, anxiety, allergic_rhinitis, reflux_esophagitis, asthma
+        FROM INPUT_DATA
+GO
 
 -- 10,000 customers -> 10,000 rows
 SELECT *FROM CUSTOMER_INFO
-
 
 -- TABLE 2
 -- from the zipcode you can identify the city, county, population, area
@@ -35,16 +60,42 @@ CREATE TABLE CUSTOMER_ADDRESS(
     -- an intresting case would be when the customer has two houses 
     -- in the same zipcode
     CONSTRAINT PK_Customer_Address PRIMARY KEY(zipcode, customer_id)
+    -- primary key makes the combination have to be unique
 )
 GO
 
 INSERT INTO CUSTOMER_ADDRESS
     -- these are the names from the INPUT_DATA
     SELECT zipcode, city, county, cstmr_state, state_population, 
-    area, customer_id FROM INPUT_DATA
+    area, customer_id 
+    FROM INPUT_DATA
+GO
 
--- Lets found out if a customer is repeated twice
+-- table 3
+-- lets verify this relation late
+CREATE TABLE CARDIAC_PATIENTS(
+    patient_id          INT IDENTITY(1,1) NOT NULL, -- start with index 1 and increment by 1
+    customer_id         VARCHAR(20)       NOT NULL  UNIQUE ,
+    highBlood          VARCHAR(15),
+    overweight      VARCHAR(15),
+    stroke          VARCHAR(15),
+    hyperlipidemia      VARCHAR(15),
 
-select distinct customer_id from CUSTOMER_INFO -- 10000 customers - so they will all have unique addresses
+    PRIMARY KEY(patient_id),
+    -- patient should not be allowed to have two records
+    FOREIGN KEY (customer_id) REFERENCES CUSTOMER_INFO(customer_id),
+    
+)
+GO
+
+INSERT INTO CARDIAC_PATIENTS
+    SELECT customer_id, highBlood, overweight, stroke, hyperlipidemia 
+    FROM CUSTOMER_INFO where hyperlipidemia='YES' OR highBlood='YES'
+GO
+
+SELECT *FROM CARDIAC_PATIENTS
+
+    
+
 
 
--- SELECTCOMMANDS.sql
+++ SELECTCOMMANDS.sql
@@ -2,3 +2,98 @@
 
 SELECT *From CUSTOMER_INFO
 SELECT *From CUSTOMER_ADDRESS
+
+select distinct diabetes from INPUT_DATA
+select distinct hyperlipidemia from INPUT_DATA
+select distinct backPain from INPUT_DATA
+select distinct anxiety from INPUT_DATA
+select distinct allergic_rhinitis from INPUT_DATA
+select distinct reflux_esophagitis from INPUT_DATA
+select distinct asthma from INPUT_DATA
+
+select distinct gender from INPUT_DATA
+
+
+select customer_id from INPUT_DATA
+
+V283504
+select *from INPUT_DATA WHERE customer_id='V283504'
+
+-- lets find the count for this
+
+--4090
+ SELECT customer_id, highBlood, overweight, stroke, hyperlipidemia 
+ FROM customer_INFO  
+    WHERE highBlood='Yes'
+
+ SELECT customer_id, highBlood, overweight, stroke, hyperlipidemia 
+ FROM customer_INFO  
+    WHERE highBlood='Yes'
+
+select customer_id, highBlood from cardiac_patients where highBlood='Yes'
+
+-- 3372
+select customer_id, highBlood, overweight, stroke, hyperlipidemia 
+FROM CUSTOMER_INFO where hyperlipidemia='YES' 
+
+--1357
+select customer_id, highBlood, overweight, stroke, hyperlipidemia 
+FROM CUSTOMER_INFO where hyperlipidemia='YES' AND highBlood='YES'
+
+-- 6105
+select customer_id, highBlood, overweight, stroke, hyperlipidemia 
+FROM CUSTOMER_INFO where hyperlipidemia='YES' OR highBlood='YES' 
+-- 6105 no age requirements here
+select customer_id, highBlood, overweight, stroke, hyperlipidemia 
+FROM cardiac_Patients where hyperlipidemia='YES' OR highBlood='YES' 
+
+select customer_id, age, highBlood, hyperlipidemia 
+    FROM CUSTOMER_INFO 
+    where   
+            (highBlood='YES' AND age>55) OR
+            (overweight ='YES' AND age>55) OR
+            (stroke = 'YES'  AND age>55) OR
+            (hyperlipidemia AND age>55)
+
+-- 5971
+select customer_id, age, highBlood, hyperlipidemia 
+    FROM CUSTOMER_INFO 
+    where   
+            (highBlood='YES' AND age>55) 
+            OR (overweight ='YES' AND age>55) 
+            OR (stroke = 'YES'  AND age>55) 
+            OR (hyperlipidemia='yes' AND age>55)
+GO
+
+select 
+    a.customer_id, a.age,
+     -- we get the customer_id to be able to use it as reference and check requirements
+    b.highBlood, b.overweight, b.stroke,  b.hyperlipidemia
+FROM
+    CUSTOMER_INFO a, CARDIAC_PATIENTS b
+WHERE
+    a.customer_id=b.customer_id AND a.age>55 
+    AND 
+    (b.highBlood='YES' OR b.overweight ='YES' 
+    OR b.stroke = 'YES' OR b.hyperlipidemia='yes')
+
+
+
+-- Generate a list of patients with customer_id ordered by age
+-- showing which is the most common disease among senior citizes
+-- Requirements: Senior citizens have to be at least 55?
+-- Generates 6451
+SELECT customer_id, age From customer_info where age>55
+
+-- we get the customer_id and the check to see if its in the cardiac patients table
+-- if it is we we check to see if it has high blood pressure
+
+-- results in 3107 rows
+SELECT a.customer_id, a.age, b.highblood From CUSTOMER_INFO a, CARDIAC_PATIENTS b
+    where  a.customer_id=b.customer_id  AND  a.age>55 AND b.highBlood='YES'
+
+-- results in 4236    
+SELECT a.customer_id, a.age, b.highblood From CUSTOMER_INFO a, CARDIAC_PATIENTS b
+    where  (a.customer_id=b.customer_id  AND  a.age>55 AND b.highBlood='YES')
+            OR (a.customer_id=b.customer_id  AND  a.age>55 AND b.hyperlipidemia='YES')
+
